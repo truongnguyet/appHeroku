f9fa0b5735e49f58355a2e43092906122c0f6899
Truong Thi Nguyet <nguyetmoon180499@gmail.com>
2021-07-28 17:16:10 +0700

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AuthGuardService} from './auth/auth-guard.service';
 import {AuthService} from './auth/auth.service';
-import {JwtHelperService, JwtModule} from '@auth0/angular-jwt';
+import {JwtHelperService, JwtModule, JwtModuleOptions} from '@auth0/angular-jwt';
 
 
 // tslint:disable-next-line:variable-name
 const JWT_Module_Options: JwtModuleOptions = {
   config: {
-    tokenGetter: 'nguyetmoon',
+    // tokenGetter: 'nguyetmoon',
     // whitelistedDomains: yourWhitelistedDomains
   }
 };
